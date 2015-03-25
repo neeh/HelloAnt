535467e8432de7dac9ed2bda5408a01a4381703f
neeh <nicolas.g@gmx.com>
2015-03-25 09:58:05 +0100

    test Javadoc (see Bot.java)

--- src/main/java/com/polytech/di4/HelloAnt/Bot.java
+++ src/main/java/com/polytech/di4/HelloAnt/Bot.java
@@ -8,6 +8,13 @@ public class Bot {
 	private Socket socket;
 	private double score;
 	
+	/**
+	 * Creates a new Bot.
+	 * @constructor
+	 * @param pseudo the pseudonym of the bot
+	 * @param socket the socket used to communicate with the bot
+	 * @param score  the current game score of the bot
+	 */
 	public Bot(String pseudo, Socket socket, double score)
 	{
 		super();
