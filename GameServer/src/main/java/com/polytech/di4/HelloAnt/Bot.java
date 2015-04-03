9d10eaeead2d642967ed4fa3090e0b9354a1208e
neeh <nicolas.g@gmx.com>
2015-04-03 22:07:51 +0200

    Bot class modified
    - bot mode
    - game infos

--- GameServer/src/main/java/com/polytech/di4/HelloAnt/Bot.java
+++ GameServer/src/main/java/com/polytech/di4/HelloAnt/Bot.java
@@ -7,63 +7,103 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The Bot class.
+ * Represents a bot in the game server.
+ * The bot can be playing a game or waiting for a game in the lobby.
  * @class
  * @author Nicolas
  */
 public class Bot
 {
 	private static final Logger LOGGER = LoggerFactory.getLogger(Bot.class);
-	private String nickname;
+	
+	/**
+	 * The nickname of the bot.
+	 * @see Documentation/protocol/nickspecs.html
+	 */
+	private String nick;
+	
+	/**
+	 * The socket used to communicate with the bot.
+	 * @see Socket
+	 */
 	private Socket socket;
+	
+	/**
+	 * The PrintWriter object used to send messages to the bot.
+	 * The object is cached here to avoid creating a PrintWriter every time we need to 
+	 * send a message to the bot.
+	 * @see PrintWriter
+	 */
 	private PrintWriter printWriter;
+	
+	/**
+	 * The gaming mode of the bot.
+	 * @see BotMode
+	 */
+	private BotMode mode;
+	
+	/**
+	 * The game in which the bot is currently in.
+	 * null if bot isn't in game.
+	 */
+	private Game game;
+	
+	/**
+	 * The general score of the bot.
+	 * The bot score is updated after a match.
+	 */
 	private double score;
 	
 	/**
 	 * Creates a new bot.
 	 * @constructor
-	 * @param pseudo the pseudonym of the bot
-	 * @param socket the socket used to communicate with the bot
-	 * @param score  the current game score of the bot
+	 * @param nick the nickname of the bot.
+	 * @param socket the socket used to communicate with the bot.
+	 * @param mode the gaming mode chosen by the bot.
+	 * @param score the current game score of the bot.
 	 */
-	public Bot(String nickname, Socket socket, double score)
+	public Bot(String nick, Socket socket, BotMode mode, double score)
 	{
-		this.nickname = nickname;
+		this.nick = nick;
+		
 		this.socket = socket;
 		printWriter = null;
+		
+		this.mode = mode;
+		this.game = null;
 		this.score = score;
 	}
 
 	/**
-	 * Gets the pseudonym of a bot.
-	 * @return the pseudonym of the bot
+	 * Gets the nickname of a bot.
+	 * @return the nickname of the bot.
 	 */
-	public String getNickname()
+	public String getNick()
 	{
-		return nickname;
+		return nick;
 	}
 
 	/**
-	 * Sets the pseudonym of a bot.
-	 * @param pseudo the new pseudonym of the bot
+	 * Sets the nickname of a bot.
+	 * @param nick the new nickname of the bot.
 	 */
-	public void setNickname(String nickname)
+	public void setNick(String nick)
 	{
-		this.nickname = nickname;
+		this.nick = nick;
 	}
 
 	/**
 	 * Gets the socket of a bot.
-	 * @return the socket used to communicate with the bot
+	 * @return the socket used to communicate with the bot.
 	 */
 	public Socket getSocket()
 	{
 		return socket;
 	}
-
+	
 	/**
 	 * Sets the socket of a bot and updates its print writer.
-	 * @param socket the new socker used to communicate with the bot
+	 * @param socket the new socket used to communicate with the bot.
 	 */
 	public void setSocket(Socket socket)
 	{
@@ -74,9 +114,9 @@ public class Bot
 			{
 				printWriter = new PrintWriter(socket.getOutputStream());
 			}
-			catch(IOException e)
+			catch (IOException e)
 			{
-				LOGGER.error("Cannot create PrintWriter for bot " + nickname);
+				LOGGER.error("Cannot create PrintWriter for bot " + nick);
 			}
 		}
 		else
@@ -84,10 +124,66 @@ public class Bot
 			printWriter = null;
 		}
 	}
+	
+	/**
+	 * Gets the gaming mode of a bot.
+	 * @return the gaming mode of the bot.
+	 * @see BotMode
+	 */
+	public BotMode getMode()
+	{
+		return mode;
+	}
+	
+	/**
+	 * Sets the gaming mode of a bot.
+	 * @param mode the new gaming mode of the bot.
+	 * @see BotMode
+	 */
+	public setBotMode(BotMode mode)
+	{
+		this.mode = mode;
+	}
+	
+	/**
+	 * Returns whether the bot is currently playing.
+	 * A bot which is not in game is waiting for a game.
+	 * @return true if the bot is in a game
+	 */
+	public Boolean isInGame()
+	{
+		return game != null;
+	}
+	
+	/**
+	 * Gets the game in which the bot is playing.
+	 * @return the game in which the bot is playing or null if the bot isn't in a game.
+	 */
+	public Game getGame()
+	{
+		return game;
+	}
+	
+	/**
+	 * Sets the game of a bot.
+	 * @param game the game the bot is in.
+	 */
+	public void setGame(Game game)
+	{
+		if (this.game == null)
+		{
+			this.game = game;
+		}
+		else
+		{
+			LOGGER.error("Attemp to enroll the bot '" + nick + "' which is already in "
+					+ "game");
+		}
+	}
 
 	/**
-	 * Gets the score of a bot.
-	 * @return the game score of the bot
+	 * Gets the general score of a bot.
+	 * @return the score of the bot.
 	 */
 	public double getScore()
 	{
@@ -95,8 +191,8 @@ public class Bot
 	}
 
 	/**
-	 * Sets the score of a bot.
-	 * @param score the new game score of the bot
+	 * Sets the general score of a bot.
+	 * @param score the new score of the bot.
 	 */
 	public void setScore(double score)
 	{
@@ -104,12 +200,11 @@ public class Bot
 	}
 	
 	/**
-	 * Send a message to a bot using its bot socket.
-	 * @param message the message to send to the bot
+	 * Send a message to a bot using its socket (via the cached PrintWriter objet).
+	 * @param message the message to send to the bot.
 	 */
 	public void send(String message)
 	{
 		printWriter.println(message);
 	}
-	
 }
---
+++ GameServer/src/main/java/com/polytech/di4/HelloAnt/BotMode.java
@@ -0,0 +1,15 @@
+package com.polytech.di4.HelloAnt;
+
+/**
+ * The gaming mode of a bot.
+ * Can be either regular or training.
+ * Training mode means the bot wants to play immediately against a computer (mainly used
+ * for debug purposes.)
+ * @enum
+ * @author Nicolas
+ */
+public enum BotMode
+{
+	REGULAR,
+	TRAINING;
+}
