eddd23ef135fbd8077e3a6c303fc4a606ee3349e
Soulburst <jonathan.lucky13@gmail.com>
2015-04-08 09:08:21 +0200

    Ajout de GameManager

---
+++ GameServer/src/main/java/com/polytech/di4/HelloAnt/GameManager.java
@@ -0,0 +1,24 @@
+package com.polytech.di4.HelloAnt;
+
+public class GameManager implements Runnable
+{
+
+	@Override
+	public void run()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * Function that permit to update the values in posToTest, so it helps explore the tree
+	 * @param posToTest array to update
+	 * @param size the size of the tree
+	 */
+	private void incrementPosToTest(int posToTest[], int size){
+		
+	}
+	
+	
+
+}
