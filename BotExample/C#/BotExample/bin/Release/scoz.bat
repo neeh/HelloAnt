d05d4bca3e0bd3912e7a5a07e160cd9feea71696
neeh <nicolas.g@gmx.com>
2015-06-24 23:04:34 +0200

    V1 delivered

--- BotExample/C#/BotExample/bin/Release/scoz.bat
+++
@@ -1,3 +0,0 @@
-start BotExample.exe 127.0.0.1:12345 a
-start BotExample.exe 127.0.0.1:12345 b
-start BotExample.exe 127.0.0.1:12345 c
---
+++ BotExample/C#/BotExample/scoz.bat
@@ -0,0 +1,4 @@
+start BotExample.exe 127.0.0.1:12345 token1
+start BotExample.exe 127.0.0.1:12345 token2
+start BotExample.exe 127.0.0.1:12345 token3
+start BotExample.exe 127.0.0.1:12345 token4
